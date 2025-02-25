fb88edb83c70ead7ad204d59218266ad56ef805d
vitalii-pc <alievruslan1818@gmail.com>
2025-02-25 21:05:21 +0100

    add section auth

--- src/components/Auth/Auth.jsx
+++ src/components/Auth/Auth.jsx
@@ -1,5 +1,37 @@
+import { LuLogIn, LuLogOut } from "react-icons/lu";
+import s from "./Auth.module.css";
+
 const Auth = () => {
-	return <div>Auth</div>;
+	const isAuth = true;
+	const login = "Abalduj";
+
+	if (!isAuth) {
+		return (
+			<ul className={s.auth_list}>
+				<li className={s.auth_item}>
+					<button type="button" className={s.login_button}>
+						<LuLogIn size={20} color={`var(--yellow)`} className={s.icon} />
+						Log in
+					</button>
+				</li>
+				<li className={s.auth_item}>
+					<button type="button" className={s.registration_button}>
+						Registration
+					</button>
+				</li>
+			</ul>
+		);
+	}
+
+	return (
+		<div className={s.header_logout}>
+			<p className={`title ${s.header_logout_text}`}>Hello, {login}!</p>
+			<button type="button" className={`${s.login_button} ${s.logout}`}>
+				<LuLogOut size={20} color={`var(--yellow)`} />
+				LogOut
+			</button>
+		</div>
+	);
 };
 
 export default Auth;
